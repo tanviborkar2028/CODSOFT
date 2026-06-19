dc9e902fb98e7ce91189d7e30eda1ab7b27e2777
tanviborkar2028 <tanviborkar5810@gmail.com>
2026-06-19 23:49:09 +0530

    Create script.js

---
+++ task3/task3/task3/script.js
@@ -0,0 +1,22 @@
+// get the display box
+var display = document.getElementById("display");
+
+function addToDisplay(value) {
+  display.value = display.value + value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
